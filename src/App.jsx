e0394091b66d3c4a5295b8ee613a9266b6f85238
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-08-01 08:13:13 +0200

    [ADDED] styling props and gradients

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
 import { CTA, Brand, NavBar } from './components'
 
+import './App.css'
+
 const App = () => {
     return (
         <div>
