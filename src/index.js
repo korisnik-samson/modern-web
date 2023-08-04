0cbb79116048c0251ed1d467bb1ddfef90b509ac
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-08-04 17:20:33 +0200

    [UPDATED] Navbar & Assets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { createRoot } from 'react-dom/client';
 
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//createRoot(document.getElementById('root')).render(<App />)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<React.StrictMode>
+    <App />
+</React.StrictMode>);
